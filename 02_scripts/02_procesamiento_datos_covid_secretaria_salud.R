de2cc059adc956d42eeed670194ae9810a085dc9
Andres-RG <lrojas26@alumnos.uaq.mx>
2025-03-12 14:35:45 -0600

    datos para cdmx

--- 02_scripts/02_procesamiento_datos_covid_secretaria_salud.R
+++ 02_scripts/02_procesamiento_datos_covid_secretaria_salud.R
@@ -6,6 +6,10 @@ library(tidyverse)
 library(dplyr)
 ##-----------------------------------------------
 # se cargan los datos
-covid.mx.ss <- read_csv("01_raw_data/COVID19MEXICO.csv") # covid mexico secretaria de salud
+covid.mx.ss <- read.csv("01_raw_data/COVID19MEXICO.csv") # covid mexico secretaria de salud
 str(covid.mx.ss)
 ##-----------------------------------------------
+# FILTRADO por estados
+covid.mx.cdmx <- covid.mx.ss %>%
+  filter(ENTIDAD_UM == 09)
+# save(covid.mx.cdmx, file = "03_out/data/covid.mx.cdmx.RData")
