ffc7b8ebf041e8dfdbb866f96bbab1e39139dfa4
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-03-11 19:59:06 -0600

    base de datos "covid_mundo"

--- 02_scripts/USA.R
+++ 02_scripts/USA.R
@@ -15,21 +15,23 @@ library(EWSmethods)
 #1.- FILTRADO DE DATOS:
 #DATOS:
   covid <- owid_covid() # de aqui se carga la base de datos de owid
-#save(objeto, "nombre del archivo.RData")
-  
-#cargar la nueva base de datos:
-covid_datos <- read.csv("01_raw_data/owid-covid-data.csv")
+save(covid, file =  "covid_mundial.RData")
+load("01_raw_data/covid_mundial.RData")
 
-str(covid_datos)
-head(covid_datos)
+
+#cargar la nueva base de datos:
+  #covid_datos <- read.csv("01_raw_data/owid-covid-data.csv")
+
+str(covid)
+head(covid)
 #FILTRADO:
-covid_usa <- covid_datos %>% filter(iso_code== "USA") #volver a cargar los datos.
+covid_usa <- covid %>% filter(iso_code== "USA") #volver a cargar los datos.
 
 df_covid_usa <- data.frame(
   time = seq(1, length(covid_usa$date), 1)     ,
   covid_usa[ , 5:16]
 )
-length(df_covid_usa$date)
+
 
 head(df_covid_usa)
 names(df_covid_usa)
@@ -40,7 +42,7 @@ ews_metrics
 
 ##
 head(df_covid_usa[rowSums(is.na(df_covid_usa)) == 0,]) ## para eliminar reglones con NA
-
+df_covid_usa<- df_covid_usa[rowSums(is.na(df_covid_usa)) == 0,]
 ################################################################################
 names(df_covid_usa[-c(1:10),c(1,3)]) #para que solo este el tiempo
 #y los nuevos casos. Lo necesario para realizar el analisis.
@@ -51,24 +53,18 @@ names(df_covid_usa[-c(1:10),c(1,3)]) #para que solo este el tiempo
 library(dplyr)
 library(zoo)
 
-summary(faltante_usa$new_cases) #tienen muchos NA :/
-datos_usa_sin_na <- df_covid_usa[!is.na(df_covid_usa$new_cases), ] #NO NA
-
-#interpolados:
-  #faltante_usa <- df_covid_usa[-c(1:10),c(1,3)] #<- na.approx(df$value)
-  #faltante_usa$new_cases_interpolate <- na.approx(faltante_usa$new_cases, rule = 2)
-  #head(faltante_usa)
-
-
-#ews_univariado_usa<- uniEWS(data = faltante_usa[ ,c(1,3)], #datos interpolados; checar esto
- #                           #algunos datos pueden NO dar la manera correcta.
-  #                             metrics =  ews_metrics, #VECTOR CON LAS METRICAS.
-   #                            method = "expanding", #VENTANA QUE SE EXPANDE
-    #                           burn_in = 10, #DE PREFERENCIA CON ESTOS DATOS.
-     #                          threshold = 2, #VARIANZAS
-      #                         tail.direction = "one.tailed") 
-#plot(ews_univariado_usa) #muy parecida a la de mexico
  
+# datos_usa_sin_na <- df_covid_usa[!is.na(df_covid_usa$new_cases), ] #NO NA
+
+ews_univariado_usa <- uniEWS(data = df_covid_usa[-c(1:10),c(1,3)],
+                               metrics =  ews_metrics, #VECTOR CON LAS METRICAS.
+                               method = "expanding", #VENTANA QUE SE EXPANDE
+                               burn_in = 10, #DE PREFERENCIA CON ESTOS DATOS.
+                               threshold = 2, #VARIANZAS
+                               tail.direction = "one.tailed") 
+plot(ews_univariado_usa) 
+
+####################################################################################
 #datos_usa_sin_na
 
 ews_univariado_usa_na<- uniEWS(data = datos_usa_sin_na[-c(1:10),c(1,3)], #datos interpolados; checar esto
