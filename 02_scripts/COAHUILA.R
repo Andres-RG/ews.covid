ccae06f68eda837fb9d5cb6aa79027fdf220bcfe
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-04-08 14:07:16 -0600

    ...

--- 02_scripts/COAHUILA.R
+++ 02_scripts/COAHUILA.R
@@ -40,14 +40,18 @@ coahuila_ews_20 <- uniEWS(data = coahuila_ewsdatos_20,
                           threshold = 2,
                           tail.direction = "one.tailed"
 )
+pdf("03_out/plots/coahuila.20.pdf", height = 8, width = 10)
 plot(coahuila_ews_20)
+dev.off()
+
 data.uni.ews(coahuila_ews_20) ->ews_coahuila_2020 #extraer los datos/funcion.
 plot.univariate.ews.ggplot(ews_coahuila_2020)-> ews_coahuila_plot_2020
 
 #OBJETO GGPLOT: 
+pdf("03_out/plots/coahuila.20.1.pdf", height = 8, width = 10)
 ews_coahuila_plot_2020 +labs(title = "COAHUILA", subtitle = "2020")+
   geom_hline(yintercept = c(2), col = "red")
-
+dev.off()
 
 #2021
 #  covid.mx.cl.2021$CLASIFICACION_FINAL
@@ -71,14 +75,18 @@ coahuila_ews_21 <- uniEWS(data = coahuila_ewsdatos_21,
                           threshold = 2,
                           tail.direction = "one.tailed"
 )
+pdf("03_out/plots/coahuila.21.pdf", height = 8, width = 10)
 plot(coahuila_ews_21)
+dev.off()
+
 data.uni.ews(coahuila_ews_21) ->ews_coahuila_2021 #extraer los datos/funcion.
 plot.univariate.ews.ggplot(ews_coahuila_2021)-> ews_coahuila_plot_2021
 
 #OBJETO GGPLOT: 
+pdf("03_out/plots/coahuila.21.1.pdf", height = 8, width = 10)
 ews_coahuila_plot_2021 +labs(title = "COAHUILA", subtitle = "2021")+
   geom_hline(yintercept = c(2), col = "red")
-
+dev.off()
 
 #2022
   covid.mx.cl.2022$CLASIFICACION_FINAL
@@ -102,14 +110,18 @@ coahuila_ews_22 <- uniEWS(data = coahuila_ewsdatos_22,
                           threshold = 2,
                           tail.direction = "one.tailed"
 )
+pdf("03_out/plots/coahuila.22.pdf", height = 8, width = 10)
 plot(coahuila_ews_22)
+dev.off()
+
 data.uni.ews(coahuila_ews_22) ->ews_coahuila_2022 #extraer los datos/funcion.
 plot.univariate.ews.ggplot(ews_coahuila_2022)-> ews_coahuila_plot_2022
 
 #OBJETO GGPLOT: 
+pdf("03_out/plots/coahuila.22.2.pdf", height = 8, width = 10)
 ews_coahuila_plot_2022 +labs(title = "COAHUILA", subtitle = "2022")+
   geom_hline(yintercept = c(2), col = "red")
-
+dev.off()
 
 
 #2023
@@ -134,13 +146,18 @@ coahuila_ews_23 <- uniEWS(data = coahuila_ewsdatos_23,
                           threshold = 2,
                           tail.direction = "one.tailed"
 )
+pdf("03_out/plots/coahuila.23.pdf", height = 8, width = 10)
 plot(coahuila_ews_23)
+dev.off()
+
 data.uni.ews(coahuila_ews_23) ->ews_coahuila_2023 #extraer los datos/funcion.
 plot.univariate.ews.ggplot(ews_coahuila_2023)-> ews_coahuila_plot_2023
 
 #OBJETO GGPLOT: 
+pdf("03_out/plots/coahuila.23.1.pdf", height = 8, width = 10)
 ews_coahuila_plot_2023 +labs(title = "COAHUILA", subtitle = "2023")+
   geom_hline(yintercept = c(2), col = "red")
+dev.off()
 
 #2024-5
   #covid.mx.cl.2024$CLASIFICACION_FINAL_COVID
@@ -164,10 +181,16 @@ coahuila_ews_24 <- uniEWS(data = coahuila_ewsdatos_24,
                           threshold = 2,
                           tail.direction = "one.tailed"
 )
+pdf("03_out/plots/coahuila.24.pdf", height = 8, width = 10)
 plot(coahuila_ews_24)
+dev.off()
+pdf("03_out/plots/coahuila.24.1.pdf", height = 8, width = 10)
+
 data.uni.ews(coahuila_ews_24) ->ews_coahuila_2024 #extraer los datos/funcion.
 plot.univariate.ews.ggplot(ews_coahuila_2024)-> ews_coahuila_plot_2024
 
 #OBJETO GGPLOT: 
+pdf("03_out/plots/coahuila.24.1.pdf", height = 8, width = 10)
 ews_coahuila_plot_2024 +labs(title = "COAHUILA", subtitle = "2024-5")+
   geom_hline(yintercept = c(2), col = "red")
+dev.off()
